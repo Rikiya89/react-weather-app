3059110c708d996fce05d66e69e0ea3ca6968a71
Rikiya89 <83910358+Rikiya89@users.noreply.github.com>
2022-10-23 18:35:17 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import './App.css';
 import Search from './components/search/search';
 import CurrentWeather from './components/current-weather/current-weather';
+import { WEATHER_API_URL,WEATHER_API_KEY } from './api';
+
 
 function App() {
 
   const handleOnSearchChange = (searchData) => {
-    console.log(searchData);
+    const [lat, lon] = searchData.value.split(" ");
+
+    const currentWeatherFeth = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`)
   }
 
   return (
     <div className="container">
       <Search onSearchChange={handleOnSearchChange} />
-      <CurrentWeather/>
+      <CurrentWeather />
     </div>
   );
 }
