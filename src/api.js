38cefc8a8d12fabad6b484ec2949273fe348f31a
Rikiya89 <83910358+Rikiya89@users.noreply.github.com>
2022-11-22 11:25:02 +0900

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,9 +1,9 @@
 export const geoApiOptions = {
-    method: 'GET',
-    headers: {
-        'X-RapidAPI-Key': 'e88b5d9dd5msh436e2b706532dc4p16a1c9jsn3b280e99e5f3',
-        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-    }
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'e88b5d9dd5msh436e2b706532dc4p16a1c9jsn3b280e99e5f3',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
 };
 
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
